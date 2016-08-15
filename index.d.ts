803d2b314ac443e0cef8593dcac8f1369272023a
hemantpopli <hemant@wizni.com>
2016-08-16 01:18:55 +0530

    exporting columns interface

--- index.d.ts
+++ index.d.ts
@@ -1,46 +1,47 @@
-interface WizeColumn {
-    name?: string,
-    displayName?: string,
-    isAvailable?: boolean,
-    letterCase: string,
-    type: string,
-    trim: boolean,
-    enum: string[],
-    required: boolean,
-    unique: boolean,
-    index: boolean,
-    fieldName?: string,
-    length?: number,
-    default: Object,
-    ref?: string,
-    readRoles?: string[],
-    createRoles?: string[],
-    updateRoles?: string[],
-    deleteRoles?: string[],
-    definedBy?: string[]
+declare module WizeSchemaMetadata { 
+    export interface WizeColumn {
+        name?: string,
+        displayName?: string,
+        isAvailable?: boolean,
+        letterCase: string,
+        type: string,
+        trim: boolean,
+        enum: string[],
+        required: boolean,
+        unique: boolean,
+        index: boolean,
+        fieldName?: string,
+        length?: number,
+        default: Object,
+        ref?: string,
+        readRoles?: string[],
+        createRoles?: string[],
+        updateRoles?: string[],
+        deleteRoles?: string[],
+        definedBy?: string[]
+    }
+    interface WizeRelation {
+        type: string,
+        schemaName: string
+    }
+    export interface WizeSchema { 
+        name: string,
+        tableName: string,
+        columns: WizeColumn[],
+        projectName: string,
+        userName: string,
+        createdAt: number,
+        lastUpdateAt: number,
+        groups: Object[],
+        dataSource: string,
+        relationships: WizeRelation[],
+        isSystemDefined: boolean,
+        versioning: boolean,
+        certification: boolean,
+        primaryKeys: string[],
+        readRoles: string[],
+        createRoles: string[],
+        updateRoles: string[],
+        deleteRoles: string[]
+    }
 }
-interface WizeRelation {
-    type: string,
-    schemaName: string
-}
-interface WizeSchema { 
-    name: string,
-    tableName: string,
-    columns: WizeColumn[],
-    projectName: string,
-    userName: string,
-    createdAt: number,
-    lastUpdateAt: number,
-    groups: Object[],
-    dataSource: string,
-    relationships: WizeRelation[],
-    isSystemDefined: boolean,
-    versioning: boolean,
-    certification: boolean,
-    primaryKeys: string[],
-    readRoles: string[],
-    createRoles: string[],
-    updateRoles: string[],
-    deleteRoles: string[]
-}
-export {WizeColumn, WizeSchema}
