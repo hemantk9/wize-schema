b8c590b5a02543eeaf2a9591c16f4e5f5aea005e
hemantpopli <hemant@wizni.com>
2016-08-16 00:44:28 +0530

    first commit with wize schema structure

---
+++ index.d.ts
@@ -0,0 +1,48 @@
+interface WizeColumn {
+    name: string,
+    displayName: string,
+    isAvailable: boolean,
+    letterCase: string,
+    type: string,
+    trim: boolean,
+    enum: string[],
+    required: boolean,
+    unique: boolean,
+    index: boolean,
+    fieldName: string,
+    length: number,
+    default: Object,
+    ref: string,
+    readRoles: string[],
+    createRoles: string[],
+    updateRoles: string[],
+    deleteRoles: string[],
+    definedBy: string[]
+}
+interface WizeRelation {
+    type: string,
+    schemaName: string
+}
+
+interface WizeSchema { 
+    name: string,
+    tableName: string,
+    columns: WizeColumn[],
+    projectName: string,
+    userName: string,
+    createdAt: number,
+    lastUpdateAt: number,
+    groups: Object[],
+    dataSource: string,
+    relationships: WizeRelation[],
+    isSystemDefined: boolean,
+    versioning: boolean,
+    certification: boolean,
+    primaryKeys: string[],
+    readRoles: string[],
+    createRoles: string[],
+    updateRoles: string[],
+    deleteRoles: string[]
+}
+
+export {WizeSchema};
